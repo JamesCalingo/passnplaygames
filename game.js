8e90c7129322ca47d9ab1a29f53e4ddb0825f95c
JC <james.calingo@gmail.com>
2019-11-04 18:26:37 -0500

    working demo

--- game.js
+++ game.js
@@ -1,9 +1,19 @@
-var playerArray = []
+var playerArray = [];
 
 $("#go").on("click", function (e){
   e.preventDefault();
   var numOfPlayers = $("#playerNum").val().trim();
+  if(numOfPlayers <= 0){
+    console.log(numOfPlayers)
+    alert("Do you seriously have no one playing?");
+    return false
+  }
+  else{
+  var numOfMafia = $("#mafiaNum").val()
   console.log(numOfPlayers);
-  playerArray.push("player", "player", "player")
-  alert(playerArray)
+  console.log(numOfMafia);
+  playerArray.push("player", "mafia", "doctor", "cop");
+  var role = playerArray[Math.floor(Math.random() * playerArray.length)];
+  alert(role);
+  }
 })
