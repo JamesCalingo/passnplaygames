73546a110b32fb88c4f830151afddcf566516396
JC <james.calingo@gmail.com>
2019-11-05 13:17:14 -0500

    mostly finished HTML

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,10 +2,10 @@ var playerArray = [];
 
 $("#goMafia").on("click", function (e){
   e.preventDefault();
-  var numOfPlayers = $("#playerNum").val().trim();
+  var numOfPlayers = $(".playerNum").val().trim();
   if(numOfPlayers <= 0){
     console.log(numOfPlayers)
-    alert("Do you seriously have no one playing?");
+    alert("Is this a ghost town?");
     return false
   }
   else{
@@ -19,7 +19,33 @@ $("#goMafia").on("click", function (e){
     playerArray.push("Mafia")
   }
   console.log(playerArray)
-  var role = playerArray[Math.floor(Math.random() * playerArray.length)];
+  let role = playerArray[Math.floor(Math.random() * playerArray.length)];
+  alert(role);
+  }
+});
+
+$("#goArtist").on("click", function (e){
+  e.preventDefault();
+  var numOfPlayers = $(".playerNum").val().trim();
+  var word = $("#fakeArtistWord").val().trim();
+  var category = $("#fakeArtistCat").val().trim()
+  if(numOfPlayers <= 0){
+    alert("How can you expect art to be produced if we have no artists?");
+    return false
+  }
+  if(!word || !category){
+    alert("How can these artists produce art if they don't know what they're supposed to produce?")
+    return false
+  }
+  else{
+  console.log(word);
+  console.log(category);
+  for(var i = 0; i < numOfPlayers - 1; i++){
+    playerArray.push(word)
+  }
+  playerArray.push("X - You are the fake artist!")
+  console.log(playerArray)
+  let role = playerArray[Math.floor(Math.random() * playerArray.length)];
   alert(role);
   }
 })
