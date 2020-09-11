3191debb7e6b69ee92dd5a0e2ed30f05dc576f8b
JC <james.calingo@gmail.com>
2020-09-11 16:45:53 -0400

    formatted js

--- fakeart/js/fakeArtist.js
+++ fakeart/js/fakeArtist.js
@@ -1,67 +1,61 @@
-var playerArray = ["X - you are the Fake Artist!"]
+const playerArray = ['X - you are the Fake Artist!'];
+let category;
 
-startGame = () =>{
+startGame = () => {
   console.log(playerArray)
-  $(".main").html(`<div class="text-center">
+  $('.main').html(`<div class="text-center">
   <h1>NOTE: make sure you are looking at THIS page before passing/when you receive the device</h1>
   
   <p>When you're ready, click the button below to find out the word...or maybe not.</p>
   
   <button class="btn btn-danger btn-lg mt-3 findRole">What's the word?</button>
   </div>`)
- 
-  }
+}
 
-
-
-$("#goArtist").on("click", function (e){
-  e.preventDefault();
-  let numOfPlayers = $(".playerNum").val().trim();
-  let word = $("#fakeArtistWord").val().trim();
-  category = $("#fakeArtistCat").val().trim()
-  if(numOfPlayers <= 0){
-    Swal.fire("How can you expect art to be produced if we have no artists?");
+$('#goArtist').on('click', function (e) {
+  e.preventDefault()
+  const numOfPlayers = $('.playerNum').val().trim()
+  const word = $('#fakeArtistWord').val().trim()
+  category = $('#fakeArtistCat').val().trim()
+  if (numOfPlayers <= 0) {
+    Swal.fire('How can you expect art to be produced if we have no artists?')
     return false
-  }else if (numOfPlayers > 15){
-    Swal.fire("Too many artists will make your canvas quite messy. Try to keep it to no more than 15 or so.");
+  } else if (numOfPlayers > 15) {
+    Swal.fire('Too many artists will make your canvas quite messy. Try to keep it to no more than 15 or so.')
     return false
-  }
-  else if(!word || !category){
+  } else if (!word || !category) {
     Swal.fire("How can these artists produce art if they don't know what they're supposed to produce?")
     return false
-  }
-  else if (word === "x" || word === "X"){
-    for(var i = 0; i < numOfPlayers - 1; i++){
-      playerArray.push("X - you are the Fake Artist!");
+  } else if (word === 'x' || word === 'X') {
+    for (var i = 0; i < numOfPlayers - 1; i++) {
+      playerArray.push('X - you are the Fake Artist!')
+    }
+    startGame()
+  } else {
+    console.log(word)
+    console.log(category)
+    for (var i = 0; i < numOfPlayers - 1; i++) {
+      playerArray.push(word)
     }
     startGame()
   }
-  else{
-  console.log(word);
-  console.log(category);
-  for(var i = 0; i < numOfPlayers - 1; i++){
-    playerArray.push(word)
-  }
-  startGame()
-  }
 })
 
-$(document).on("click", ".findRole", function (e){
-  e.preventDefault();
-  if(playerArray.length === 0){
-    window.location.href = "../index.html"
-  }
-  else{
-let randNum = Math.floor(Math.random() * playerArray.length)
-let selection = playerArray[randNum]
-Swal.fire({
-  title: `The word is...\n${selection}`,
-  text: `Category: ${category}`,
-  confirmButtonText: "Whenever you're ready, click here before passing to the next player."
-})
+$(document).on('click', '.findRole', function (e) {
+  e.preventDefault()
+  if (playerArray.length === 0) {
+    window.location.href = '../index.html'
+  } else {
+    const randNum = Math.floor(Math.random() * playerArray.length)
+    const selection = playerArray[randNum]
+    Swal.fire({
+      title: `The word is...\n${selection}`,
+      text: `Category: ${category}`,
+      confirmButtonText: "Whenever you're ready, click here before passing to the next player."
+    })
 
-// $("#category").text(category)
-playerArray.splice(randNum, 1)
-console.log(playerArray)
+    // $("#category").text(category)
+    playerArray.splice(randNum, 1)
+    console.log(playerArray.length)
   }
 })
--- mafia/js/mafia.js
+++ mafia/js/mafia.js
@@ -1,89 +1,84 @@
-const playerArray = [];
+const playerArray = []
 // objects for the array (in the future they will be better scoped)
 var playerObj = {
-  name: "Civilian",
-  role: `You're just a regular civilian. Try to get the mafia voted out, and if they're all gone, you win!\nWhen you're ready, click the button below and pass the device to the next player.`
-};
+  name: 'Civilian',
+  role: 'You\'re just a regular civilian. Try to get the mafia voted out, and if they\'re all gone, you win!\nWhen you\'re ready, click the button below and pass the device to the next player.'
+}
 var mafiaObj = {
-  name: "Mafia",
+  name: 'Mafia',
   role: "You're part of the mafia. Eliminate the townspeople one by one during night and day, and once enough of the town is gone, you win! \nWhen you're ready, click the button below and pass the device to the next player."
-};
+}
 var detectiveObj = {
-  name: "Detective",
+  name: 'Detective',
   role: "You are a special detective in charge of the mafia case. Each night, you will inspect one player to see if they are with the mafia. You're on the village team, so use your knowledge to your advantage, eliminiate the mafia, and you win! \nWhen you're ready, click the button below and pass the device to the next player."
-};
+}
 var medicObj = {
-  name: "Medic",
+  name: 'Medic',
   role: "You're a highly skilled medic. Each night, you will choose one player(you are allowed to choose yourself), and if they are attacked by the mafia, you will prevent their death with your epic medic skills. You're on the village team, so help eliminate the mafia and you win! \nWhen you're ready, click the button below and pass the device to the next player."
-};
+}
 var cupidObj = {
-  name: "Cupid",
+  name: 'Cupid',
   role: "You're a legendary matchmaker. On the first night, you will choose two people to become lovers. After this, you're just an ordinary civilian, so eliminate the mafia (even if one of them is in love) and you win!\nWhen you're ready, click the button below and pass the device to the next player."
-};
+}
 var tannerObj = {
-  name: "Tanner",
+  name: 'Tanner',
   role: "You're a local tanner...but you're not fond of it. You'd rather be literallly anywhere else but this town right now, but there's one problem: the town kinda needs you to stay at your job. However, with the mafia confusion going around, you figure it's a good time to try and trick the town to buying your one way ticket out of town! \ntl;dr: Your goal is to get voted out by town. If this happens, you win! Note that you do NOT win if the mafia kills you at night, and if you're chosen by Cupid, you do not fall in love.\nWhen you're ready, click the button below and pass the device to the next player."
 }
 
+$('form').on('submit', function (e) {
+  e.preventDefault()
+  var numOfPlayers = $('.playerNum').val().trim()
 
-
-$("form").on("submit", function (e){
-  e.preventDefault();
-  var numOfPlayers = $(".playerNum").val().trim();
-  
-  if(numOfPlayers <= 0){
+  if (numOfPlayers <= 0) {
     console.log(numOfPlayers)
-    Swal.fire("There's seriously no one in this town? Is this a ghost town?");
+    Swal.fire("There's seriously no one in this town? Is this a ghost town?")
     return false
-  }
-  else if(numOfPlayers > 100){
+  } else if (numOfPlayers > 100) {
     Swal.fire({
-      title: "WHOA BUDDY!",
+      title: 'WHOA BUDDY!',
       text: "You can't have THAT many people playing with you...right? "
     })
     return false
-  }
-  else{
+  } else {
     // Get the number of Mafia and special roles
-  var numOfMafia = $("#mafiaNum").val();
-  var detCheck = document.getElementById("detectiveCheck");
-  var medCheck = document.getElementById("medicCheck");
-  var cupidCheck = document.getElementById("cupidCheck");
-  var tannerCheck = document.getElementById("tannerCheck")
-  console.log(numOfPlayers);
-  console.log(numOfMafia);
+    var numOfMafia = $('#mafiaNum').val()
+    var detCheck = document.getElementById('detectiveCheck')
+    var medCheck = document.getElementById('medicCheck')
+    var cupidCheck = document.getElementById('cupidCheck')
+    var tannerCheck = document.getElementById('tannerCheck')
+    console.log(numOfPlayers)
+    console.log(numOfMafia)
 
-  // Create regular player object and add them too array
-  
-  for(var i = 0; i < numOfPlayers - numOfMafia; i++){
-    playerArray.push(playerObj)
-  };
-  // Create mafia object and add to array
- 
-  for(var j = 0; j < numOfMafia; j++){
-    playerArray.push(mafiaObj);
-  };
+    // Create regular player object and add them too array
 
-  // special roles
-  if(detCheck.checked == true){
-    playerArray.push(detectiveObj);
-    playerArray.shift()
-  };
-  if(medCheck.checked == true){
-    playerArray.push(medicObj);
-    playerArray.shift()
-  };
-  if(cupidCheck.checked == true){
-    playerArray.push(cupidObj);
-    playerArray.shift()
-  };
-  if(tannerCheck.checked == true){
-    playerArray.push(tannerObj);
-    playerArray.shift()
-  };
-  console.log(playerArray);
-  // sessionStorage.setItem("players", JSON.stringify(playerArray))
-$(".main").html(`<div class="text-center">
+    for (var i = 0; i < numOfPlayers - numOfMafia; i++) {
+      playerArray.push(playerObj)
+    };
+    // Create mafia object and add to array
+
+    for (var j = 0; j < numOfMafia; j++) {
+      playerArray.push(mafiaObj)
+    };
+
+    // special roles
+    if (detCheck.checked === true) {
+      playerArray.push(detectiveObj)
+      playerArray.shift()
+    };
+    if (medCheck.checked === true) {
+      playerArray.push(medicObj)
+      playerArray.shift()
+    };
+    if (cupidCheck.checked === true) {
+      playerArray.push(cupidObj)
+      playerArray.shift()
+    };
+    if (tannerCheck.checked === true) {
+      playerArray.push(tannerObj)
+      playerArray.shift()
+    };
+    console.log(playerArray)
+    $('.main').html(`<div class="text-center">
 <h1>NOTE: Make sure you are looking at THIS page when you receive the device</h1>
 
 <p>Hello player.</p>
@@ -95,49 +90,23 @@ $(".main").html(`<div class="text-center">
 <button class="btn btn-lg btn-danger findRole">I'm ready! Let's go!</button>
 </div>
 `)
-  // window.location.href = "player.html"
   }
-  $("document").on("click", ".findRole", function (e){
-    e.preventDefault();
-    console.log("I was clicked!")
-    // let players=sessionStorage.getItem("players")
-    if(playerArray.length === 0){
-      window.location.href = "../index.html"
-    }
-    else{
-      
-  let randNum = Math.floor(Math.random() * playerArray.length)
-  let selection = playerArray[randNum]
-  Swal.fire({
-    title: `You are...\n ${selection.name}`,
-    text: selection.role,
-    confirmButtonText: "HI I'M THE BUTTON"
-  })
-  playerArray.splice(randNum, 1);
-  console.log(playerArray)
-    }
-  })
-  
-});
-
-;
-$(document).on("click", ".findRole", function (e){
-  e.preventDefault();
-  console.log("I was clicked!")
-  if(playerArray.length === 0){
-    window.location.href = "../index.html"
-  }
-  else{
-    
-let randNum = Math.floor(Math.random() * playerArray.length)
-let selection = playerArray[randNum]
-
-Swal.fire({
-  title: `You are the...\n ${selection.name}`,
-  text: selection.role,
-  confirmButtonText: "HI I'M THE BUTTON"
 })
-playerArray.splice(randNum, 1);
-console.log(playerArray)
+
+$(document).on('click', '.findRole', function (e) {
+  e.preventDefault()
+  if (playerArray.length === 0) {
+    window.location.href = '../index.html'
+  } else {
+    const randNum = Math.floor(Math.random() * playerArray.length)
+    const selection = playerArray[randNum]
+
+    Swal.fire({
+      title: `You are the...\n ${selection.name}`,
+      text: selection.role,
+      confirmButtonText: "HI I'M THE BUTTON"
+    })
+    playerArray.splice(randNum, 1)
+    console.log(playerArray.length)
   }
 })
